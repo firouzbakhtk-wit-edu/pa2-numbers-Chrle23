bb724b038af27b8c90d044912806ded890070fcc
Chrle23 <Barrettc3@wit.edu>
2024-10-09 11:23:11 -0400

    done with b

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -20,16 +20,20 @@ public class PA2b {
 			double discrinminant = (b * b) - (4 * a * c);
 			// this is the forumla of calculating discriminant
 			
+			//chekc if discrinminant is less than 0
 			if (discrinminant < 0) {
 				System.out.println("Roots: imaginary");
 			} else if (discrinminant == 0) {
+				// this would check if its equal to 0 and to print out the single root accordingly
 				double root =-b / (2 *a);
 				System.out.printf("Root: %.2f%n", root);
 			} else {
+				// this would check if is positive if we made it this far and finding out the 2 roots
 				double sqrtDiscrinminant = Math.sqrt(discrinminant);
 				double root1 = (-b - sqrtDiscrinminant) / (2 * a);
 				double root2 = (-b + sqrtDiscrinminant) / (2 * a);
 				
+				// reording the roots so that root 1 is less than or equal to root 2
 				if (root1 > root2) {
 					double temp = root1;
 					root1 = root2;
